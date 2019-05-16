848d5f2822ad356e4e46881b71f01f7d09e2d7cf
jiaming.yang <jiaming.yang@tophant.com>
2019-05-16 22:00:35 +0800

    cesas

--- App/Common/Service/WeChat/Index.php
+++ App/Common/Service/WeChat/Index.php
@@ -86,4 +86,54 @@ class Index
         $accessCheck->setSignature($params['signature']);
         return $wechat->officialAccount()->server()->accessCheck($accessCheck);
     }
+    public function menu()
+    {
+        $wechat = new WeChat();
+        $wechat->officialAccount()->getConfig()
+            ->setAppId('wxb7533d90d0055543')
+            ->setAppSecret('cf72d03538c1713562e2b75823eac2b4')
+            ->setToken('EasySwoole');
+        $buttons = [
+            [
+                "type" => "click",
+                "name" => "今日歌曲",
+                "key"  => "V1001_TODAY_MUSIC"
+            ],
+            [
+                "name"       => "菜单",
+                "sub_button" => [
+                    [
+                        "type" => "view",
+                        "name" => "搜索",
+                        "url"  => "http://www.soso.com/"
+                    ],
+                    [
+                        "type" => "view",
+                        "name" => "视频",
+                        "url"  => "http://v.qq.com/"
+                    ],
+                    [
+                        "type" => "click",
+                        "name" => "赞一下我们",
+                        "key" => "V1001_GOOD"
+                    ],
+                ],
+            ],
+        ];
+
+        $matchRule = [
+            "tag_id" => "2",
+            "sex" => "1",
+            "country" => "中国",
+            "province" => "广东",
+            "city" => "广州",
+            "client_platform_type" => "2",
+            "language" => "zh_CN"
+        ];
+
+        // create menu
+        //$wechat->officialAccount()->menu()->create($buttons);
+        // create conditional menu
+        $menuId = $wechat->officialAccount()->menu()->create($buttons, $matchRule);
+    }
 }
