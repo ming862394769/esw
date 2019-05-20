bbf73fa6a73475894bfe38682454027529a14bf7
jiaming.yang <jiaming.yang@tophant.com>
2019-05-20 13:48:02 +0800

    配置

--- App/HttpController/Home/Index.php
+++ App/HttpController/Home/Index.php
@@ -13,20 +13,6 @@ use easySwoole\Cache\Cache;
 
 class Index extends BaseController
 {
-    function onRequest(?string $action): ?bool
-    {
-        if (parent::onRequest($action)) {
-            //判断是否登录
-            $redis = RedisPool::defer();
-            if ($redis->get('isLogin')) {
-                $this->response()->redirect("/home/login");
-                //$this->writeJson(Status::CODE_UNAUTHORIZED, '', '登入已过期');
-                return false;
-            }
-            return true;
-        }
-        return false;
-    }
     public function index()
     {
         $this->fetch('index');
